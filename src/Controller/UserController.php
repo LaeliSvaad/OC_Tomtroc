a0ec7b5ca4c4729feab66f1829dc9993d5e44de1
LaeliSvaad <ls@laelisvaad.com>
2026-03-16 13:59:34 +0100

    user controller is clean

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,31 +1,18 @@
 <?php
 namespace App\Controller;
 
-use App\Manager\UserManager;
-use App\Manager\ChatManager;
-use App\Manager\ConversationManager;
-use App\Manager\LibraryManager;
 use App\Service\UserService;
-use App\Model\Chat;
 use App\Http\Session\SessionStorageInterface;
-use App\Http\Request;
 use App\Utils\Utils;
-use App\Utils\UserInput;
 use App\View\View;
 
 class UserController extends AbstractController
 {
-    private readonly UserManager $userManager;
-    private Request $request;
     private UserService $userService;
-    public function __construct(View $view, SessionStorageInterface $session, Request $request)
+    public function __construct(View $view, SessionStorageInterface $session)
     {
-        $this->userManager = new userManager();
         $this->userService = new UserService();
-        $this->request = $request;
-
         parent::__construct($view, $session);
-
     }
 
     public function logIn(): void
@@ -54,8 +41,8 @@ class UserController extends AbstractController
     }
 
     public function logOut(){
-        $_SESSION = [];
 
+        $_SESSION = [];
         //Suppression de la session côté client
         if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
@@ -72,125 +59,33 @@ class UserController extends AbstractController
 
     public function showPublicUserPage(int $userId): void
     {
-       if(is_null($userId)){
-           $this->render("Erreur","404-error");
+       if(!is_null($userId)){
+           $user = $this->userService->handlePublicUser($userId);
+           if (!is_null($user))
+               $this->render("Utilisateur", "user-public-account", ['user' => $user]);
        }
        else
-       {
-           $user = $this->userManager->getPublicUserById($userId);
-           if (!is_null($user)) {
-               $libraryManager = new LibraryManager();
-               $userLibrary = $libraryManager->getLibraryByUserId($userId);
-               $user->setLibrary($userLibrary);
-               $chat = new Chat();
-               if (isset($_SESSION["user"])) {
-                   $conversationManager = new ConversationManager();
-                   $conversation = $conversationManager->getConversationByUsersId($_SESSION["user"], $user->getUserId());
-                   $chat->addConversation($conversation);
-               }
-               $user->setChat($chat);
-           }
+           $this->render("Erreur","404-error");
 
-           $this->render("Utilisateur", "user-public-account", ['user' => $user]);
-       }
     }
 
     public function showPrivateUserPage(): void
     {
-        if(is_null($this->session->get('userId')))
+        if(!is_null($this->session->get('userId')))
         {
-            $this->render("Erreur", "404-error");
+            $userId =$this->session->get('userId');
+            $user = $this->userService->handlePrivateUser($userId);
+            $this->render("Utilisateur", "user-private-account", ['user' => $user, 'current_page' => 'user-private-account',]);
         }
         else
         {
-            $userId =$this->session->get('userId');
-            $user = $this->userManager->getPrivateUserById($userId);
-
-            if(!is_null($user))
-            {
-                $libraryManager = new LibraryManager();
-                $userLibrary = $libraryManager->getLibraryByUserId($userId);
-                $user->setLibrary($userLibrary);
-                $chatManager = new ChatManager();
-                $userChat = $chatManager->getChat($userId);
-                $user->setChat($userChat);
-            }
-            $this->render("Utilisateur", "user-private-account", ['user' => $user, 'current_page' => 'user-private-account',]);
+            $this->render("Erreur", "404-error");
         }
     }
 
     public function modifyUser(): void
     {
-        $userRequest["picture"] = $this->request->file("picture");
-        $userRequest["nickname"] = $this->request->post("nickname");
-        $userRequest["email"] = $this->request->post("email");
-        $userRequest["password"] = $this->request->post("password");
-        $userRequest["userId"]= $this->request->post("userId");
-
-        if(!isset($userRequest["userId"]) || $userRequest["userId"] == 0)
-            throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 0");
-        else{
-            $userRequest["userId"] = (int)UserInput::controlUserInput($this->request->post("userId"));
-            $userManager = new UserManager();
-            $user = $userManager->getPrivateUserById($userRequest["userId"]);
-        }
-
-        foreach ($userRequest as $key => $value) {
-
-            switch ($key) {
-                case 'nickname':
-                    if($userRequest["nickname"] != null)
-                    {
-                        $userRequest["nickname"] = UserInput::controlUserInput($userRequest["nickname"]);
-                        if($userRequest["nickname"] != $user->getNickname()){
-                            $modif = $userManager->modifyUserNickname($userRequest["nickname"], $userRequest["userId"]);
-                            if($modif == 0)
-                                throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 1");
-                        }
-                    }
-                    continue 2;
-
-                case 'email':
-                    if($userRequest["email"] != null && $userRequest["email"] != $user->getEmail())
-                    {
-                        $userRequest["email"] = UserInput::controlUserInput($userRequest["email"]);
-                        if($userRequest["email"] != $user->getEmail()){
-                            $modif = $userManager->modifyUserEmail($userRequest["email"], $userRequest["userId"]);
-                            if($modif == 0)
-                                throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 2");
-                        }
-                    }
-                    continue 2;
-
-                case 'password':
-                    if($userRequest["password"] != null)
-                    {
-                        $userRequest["password"] = UserInput::controlPassword($userRequest["password"]);
-                        if($userRequest["password"] != $user->getPassword()){
-                            $modif = $userManager->modifyUserPassword($userRequest["password"], $userRequest["userId"]);
-                            if($modif == 0)
-                                throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 3");
-                        }
-                    }
-                    continue 2;
-
-                case 'picture':
-                    if($userRequest["picture"] != null)
-                    {
-                        $userRequest["picture"]["name"] = UserInput::controlProfilePicture($userRequest["picture"]["name"]);
-
-                        if(move_uploaded_file($userRequest["picture"]["tmp_name"], $userRequest["picture"]["name"]) === false){
-                            throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 4");
-                        }
-                        else{
-                            $modif = $userManager->modifyUserPicture($userRequest["picture"]["name"], $userRequest["userId"]);
-                                if($modif == 0)
-                                    throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 5");
-                            }
-                    }
-                    break;
-            }
-        }
-        Utils::redirect('mon-compte');
+       $this->userService->handleModifyUser();
+       Utils::redirect('mon-compte');
     }
 }
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -3,21 +3,31 @@ namespace App\Service;
 
 use App\Http\Request;
 use App\Http\Session\SessionStorageInterface;
+use App\Manager\ChatManager;
+use App\Manager\ConversationManager;
+use App\Manager\LibraryManager;
 use App\Manager\UserManager;
+use App\Model\Chat;
 use App\Utils\UserInput;
 use App\Model\User;
 
 class UserService
 {
     private UserManager $userManager;
+    private ChatManager $chatManager;
+    private ConversationManager $conversationManager;
+    private LibraryManager $libraryManager;
     private Request $request;
     private User $user;
 
     public function __construct()
     {
         $this->userManager = new UserManager();
-        $this->request = new Request();
+        $this->libraryManager = new LibraryManager();
+        $this->chatManager = new ChatManager();
+        $this->conversationManager = new ConversationManager();
         $this->user = new User();
+        $this->request = new Request();
     }
 
     public function handleLogIn(SessionStorageInterface $session) : bool
@@ -58,5 +68,107 @@ class UserService
             }
         }
         return false;
+    }
+
+    public function handlePublicUser(int $userId) : User
+    {
+        $this->user = $this->userManager->getPublicUserById($userId);
+
+        if (!is_null($this->user)) {
+            $this->user->setLibrary($this->libraryManager->getLibraryByUserId($userId));
+            $chat = new Chat();
+            if (isset($_SESSION["user"])) {
+                $conversation = $this->conversationManager->getConversationByUsersIds($_SESSION["user"], $this->user->getUserId());
+                $chat->addConversation($conversation);
+            }
+            $this->user->setChat($chat);
+        }
+
+        return $this->user;
+    }
+
+    public function handlePrivateUser(int $userId) : User
+    {
+        $this->user = $this->userManager->getPrivateUserById($userId);
+
+        if(!is_null($this->user))
+        {
+            $this->user->setLibrary($this->libraryManager->getLibraryByUserId($userId));
+            $this->user->setChat($this->chatManager->getChat($userId));
+        }
+        return $this->user;
+    }
+
+    public function handleModifyUser() : bool
+    {
+        $userRequest["picture"] = $this->request->file("picture");
+        $userRequest["nickname"] = $this->request->post("nickname");
+        $userRequest["email"] = $this->request->post("email");
+        $userRequest["password"] = $this->request->post("password");
+        $userRequest["userId"]= $this->request->post("userId");
+
+        if(!isset($userRequest["userId"]) || $userRequest["userId"] == 0){
+            return false;
+        }
+        else{
+            $userRequest["userId"] = (int)UserInput::controlUserInput($this->request->post("userId"));
+            $this->user = $this->userManager->getPrivateUserById($userRequest["userId"]);
+            foreach ($userRequest as $key => $value) {
+                switch ($key) {
+                    case 'nickname':
+                        if($userRequest["nickname"] != null)
+                        {
+                            $userRequest["nickname"] = UserInput::controlUserInput($userRequest["nickname"]);
+                            if($userRequest["nickname"] != $this->user->getNickname()){
+                                $modif = $this->userManager->modifyUserNickname($userRequest["nickname"], $userRequest["userId"]);
+                                if($modif == 0)
+                                    throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 1");
+                            }
+                        }
+                        continue 2;
+
+                    case 'email':
+                        if($userRequest["email"] != null && $userRequest["email"] != $this->user->getEmail())
+                        {
+                            $userRequest["email"] = UserInput::controlUserInput($userRequest["email"]);
+                            if($userRequest["email"] != $this->user->getEmail()){
+                                $modif = $this->userManager->modifyUserEmail($userRequest["email"], $userRequest["userId"]);
+                                if($modif == 0)
+                                    throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 2");
+                            }
+                        }
+                        continue 2;
+
+                    case 'password':
+                        if($userRequest["password"] != null)
+                        {
+                            $userRequest["password"] = UserInput::controlPassword($userRequest["password"]);
+                            if($userRequest["password"] != $this->user->getPassword()){
+                                $modif = $this->userManager->modifyUserPassword($userRequest["password"], $userRequest["userId"]);
+                                if($modif == 0)
+                                    throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 3");
+                            }
+                        }
+                        continue 2;
+
+                    case 'picture':
+                        if($userRequest["picture"] != null)
+                        {
+                            $userRequest["picture"]["name"] = UserInput::controlProfilePicture($userRequest["picture"]["name"]);
+
+                            if(move_uploaded_file($userRequest["picture"]["tmp_name"], $userRequest["picture"]["name"]) === false){
+                                throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 4");
+                            }
+                            else{
+                                $modif = $this->userManager->modifyUserPicture($userRequest["picture"]["name"], $userRequest["userId"]);
+                                if($modif == 0)
+                                    throw new \Exception("Une erreur est survenue lors de la mise à jour de vos informations. 5");
+                            }
+                        }
+                        break;
+                }
+            }
+            return true;
+        }
     }
 }
